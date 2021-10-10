8b44c4bb0c756509d77be664b15aab9b8cf03591
Rakeshgombi <rakeshgombi18@gmail.com>
2021-10-10 20:30:35 +0530

    fetching notes from context

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import noteContext from '../Context/noteContext';
 
 const Home = () => {
+  const context = useContext(noteContext)
+  const { notes, setnotes } = context;
   return (
     <div>
       <div className="container my-4 px-5">
@@ -10,13 +13,16 @@ const Home = () => {
             <label htmlFor="InputText" className="form-label">Enter the Title</label>
             <input type="text" className="form-control" id="InputText" aria-describedby="InputText" />
           </div>
-          <div class="mb-3">
-            <label for="InputDesc" class="form-label">Example textarea</label>
-            <textarea class="form-control" id="InputDesc" rows="2"></textarea>
+          <div className="mb-3">
+            <label htmlFor="InputDesc" className="form-label">Example textarea</label>
+            <textarea className="form-control" id="InputDesc" rows="2"></textarea>
           </div>
           <button type="submit" className="btn btn-primary">Submit</button>
         </form>
         <h1>Your Notes</h1>
+        {notes.map((note) => {
+          return note.title
+        })}
       </div>
     </div>
   )
--- src/Context/NoteState.js
+++ src/Context/NoteState.js
@@ -1,13 +1,33 @@
 import NoteContext from './noteContext';
-// import { useState } from 'react';
+import { useState } from 'react';
 
 const NoteState = (props) => {
 
+  const notesInitial = [
+    {
+      "_id": "6161cebc33c4de8e227620b5",
+      "user": "613df60f283cfcabef3aac4f",
+      "title": "My turtle",
+      "description": "This is a description",
+      "tag": "Testing",
+      "date": "2021-10-09T17:17:48.920Z",
+      "__v": 0
+    },
+    {
+      "_id": "6161cec833c4de8e227620b7",
+      "user": "613df60f283cfcabef3aac4f",
+      "title": "My turtle part 2",
+      "description": "This is a description part 2",
+      "tag": "Testing",
+      "date": "2021-10-09T17:18:00.544Z",
+      "__v": 0
+    }
+  ]
+const [notes, setnotes] = useState(notesInitial)
   return (
-    <NoteContext.Provider>
+    <NoteContext.Provider value={{notes, setnotes}}>
       {props.children}
     </NoteContext.Provider>
-
   )
 }
 
